edff9dedfee0d59604b3a615fe3e0dd0d1846527
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-03 09:10:20 +0400

    fix big in notes column declaration

--- models.py
+++ models.py
@@ -13,5 +13,5 @@ class Pet(db.Model):
     species = db.Column(db.Text, nullable=False)
     photo_url = db.Column(db.Text)
     age = db.Column(db.Integer)
-    notes = db.Text(db.Text)
+    notes = db.Column(db.Text)
     available = db.Column(db.Boolean, nullable=False, default=True)
