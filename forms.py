8718308f2d655e08dc1678acc6d6df29f63c9f5b
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-04 13:02:06 +0400

    add validations for species, photo_url, and age

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, TextAreaField
-from wtforms.validators import InputRequired, Optional
+from wtforms.validators import InputRequired, Optional, URL, NumberRange, AnyOf
 
 
 class AddPetForm(FlaskForm):
@@ -9,7 +9,9 @@ class AddPetForm(FlaskForm):
     name = StringField('Pet Name', validators=[InputRequired(
         message='Pet name cannot be blank.')])
     species = StringField('Species', validators=[InputRequired(
-        message='Species cannot be blank.')])
-    photo_url = StringField('Photo URL', validators=[Optional()])
-    age = IntegerField('Age', validators=[Optional()])
+        message='Species cannot be blank.'), AnyOf(values=['cat', 'dog', 'porcupine'], message='Species must be either cat, dog, or porcupine.')])
+    photo_url = StringField('Photo URL', validators=[
+                            Optional(), URL(message='Invalid URL format.')])
+    age = IntegerField('Age', validators=[Optional(), NumberRange(
+        min=0, max=30, message='Age must be between 0 and 30.')])
     notes = TextAreaField('Notes', validators=[Optional()])
