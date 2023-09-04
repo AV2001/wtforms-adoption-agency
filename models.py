7e3106b5e94f97426899a8c9167df5308eae1fd1
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-04 12:47:27 +0400

    add default value to photo_url

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.Text, nullable=False)
     species = db.Column(db.Text, nullable=False)
-    photo_url = db.Column(db.Text)
+    photo_url = db.Column(db.Text, default='https://shorturl.at/nuNX3')
     age = db.Column(db.Integer)
     notes = db.Column(db.Text)
     available = db.Column(db.Boolean, nullable=False, default=True)
