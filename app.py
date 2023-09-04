e4234e4f8a198bb8b177cf9ca11126af89e27a9e
AV2001 <anirudhvadlamani2001@gmail.com>
2023-09-04 12:49:23 +0400

    create route to add pet and render add pet form

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, Pet
+from forms import AddPetForm
 
 # Create new instance of Flask application.
 app = Flask(__name__)
@@ -23,3 +24,22 @@ def home_page():
     '''List all pets.'''
     pets = Pet.query.all()
     return render_template('index.html', pets=pets)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_pet():
+    '''Add a pet to the database.'''
+    form = AddPetForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data if form.photo_url.data else 'https://shorturl.at/nuNX3'
+        age = form.age.data
+        notes = form.notes.data
+        pet = Pet(name=name, species=species,
+                  photo_url=photo_url, age=age, notes=notes)
+        db.session.add(pet)
+        db.session.commit()
+        return redirect('/')
+    else:
+        return render_template('add-pet.html', form=form)
